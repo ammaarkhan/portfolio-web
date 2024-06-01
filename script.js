a6249a74e029d261c917fe95c0601dfbfd41c65f
ammaarkhan <ammaarkhan03@gmail.com>
2024-06-01 07:18:09 -0700

    apply prettier formatting

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const audio = document.getElementById('background-music');
-    const descriptionSection = document.querySelector('.description-section');
-    const playButton = document.getElementById('play-button');
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.getElementById("background-music");
+  const descriptionSection = document.querySelector(".description-section");
+  const playButton = document.getElementById("play-button");
 
-    let isPlaying = false; 
+  let isPlaying = false;
 
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.5 
-    };
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
 
-    const observerCallback = (entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting && isPlaying) {
-                audio.play();
-            } else {
-                audio.pause();
-            }
-        });
-    };
-
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
-    observer.observe(descriptionSection);
-
-    playButton.addEventListener('click', function () {
-        if (isPlaying) {
-            audio.pause();
-            playButton.innerText = 'Click here for an enhanced viewing experience';
-        } else {
-            audio.play().then(() => {
-                playButton.innerText = 'Click here to reach the floor';
-            }).catch((error) => {
-                console.error('Error playing audio:', error);
-            });
-        }
-        isPlaying = !isPlaying;
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && isPlaying) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  observer.observe(descriptionSection);
+
+  playButton.addEventListener("click", function () {
+    if (isPlaying) {
+      audio.pause();
+      playButton.innerText = "click here for an enhanced viewing experience";
+    } else {
+      audio
+        .play()
+        .then(() => {
+          playButton.innerText = "click here to reach the floor";
+        })
+        .catch((error) => {
+          console.error("Error playing audio:", error);
+        });
+    }
+    isPlaying = !isPlaying;
+  });
 });
